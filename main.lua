1546c26ae284d4e164b062dc93f710bd3181f976
barryjensen <145060418+barryjensen@users.noreply.github.com>
2025-06-14 20:08:16 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -57,7 +57,7 @@ local function smallServer()
     end
 end
 
-local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/main/source.lua", true))()
+local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/main/source.lua", true))()
 
 local Window = Luna:CreateWindow({
     Name = "Saturn Hub",
